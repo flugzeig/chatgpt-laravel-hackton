fee9b1057918064f5e92507e5d07cf0fa1d1edd5
flugzeig <vova_b_tahke1337@mail.ru>
2023-05-26 20:05:20 +0300

    добавил миграцию

---
+++ database/migrations/2023_05_26_155722_create_histories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('question');
+            $table->string('response');
+            $table->unsignedInteger('chat_id');
+            $table->timestamps();
+
+            // Добавление внешних ключей
+            $table->foreign('chat_id')->references('id')->on('chats');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('histories');
+    }
+};
